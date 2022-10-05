bb7c478b052039b9f9f049e336c8e949c816b720
dozagirugi <37648180+kimdoyeonindaeyo@users.noreply.github.com>
2022-10-05 15:42:05 +0900

    입력한 도서 정보 출력하기

---
+++ _0921_book.java
@@ -0,0 +1,22 @@
+package java_study;
+
+public class _0921_book {
+
+	
+// 입력한 책 정보 출력하기
+	
+
+	public static void main(String[] args) {
+
+		String title = "문제해결기법";
+		int publication_year = 2022;
+		double weight = 857.2;
+		boolean supplement_YN = false; // 보충 자료의 여부 
+		char language = 'K'; // 언어가 한국어 
+		System.out.println(title + "," + publication_year + "," + weight + "," + supplement_YN + "," + language); 
+		// 출력하고 싶은 자료형이 많다면 '+'로 이어주기 
+		
+	}
+	
+	
+}
