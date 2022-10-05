452152f902914f5f7b14ed7d66817e16a61d45b3
dozagirugi <37648180+kimdoyeonindaeyo@users.noreply.github.com>
2022-10-05 18:57:46 +0900

    Update Dog_Animal.java

--- Dog_Animal.java
+++ Dog_Animal.java
@@ -12,13 +12,17 @@ class Dog extends Animals {
  
  // 클래스 상속을 위해서 extends라는 키워드를 사용했다.
  // Dog 클래스에는 name 이라는 객체 변수와 setName 이라는 메소드가 없지만 Animal 클래스를 상속 받았기 때문에 모두 사용이 가능하다. 	
-
+	
+	void sleep() {
+		System.out.println(this.name + " zzz...");
+	}
 }
 
 public class Dog_Animal {
 	public static void main(String[] args) {
 		Dog dog = new Dog();
 		dog.setName("baduk");
-		System.out.println(dog.name);
+		System.out.println(dog.name); // baduk 출력 
+		dog.sleep(); // baduk zzz... 출력 
 	}
 }
