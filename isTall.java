9b26c3da15b15b1b9fe06e729030bcb95e30453c
dozagirugi <37648180+kimdoyeonindaeyo@users.noreply.github.com>
2022-10-05 17:21:39 +0900

    if문 ("height가 base보다 키가 큽니다.")

---
+++ isTall.java
@@ -0,0 +1,16 @@
+package java_study;
+
+public class isTall {
+
+	public static void main(String[] args) {
+		int base = 180;
+		int height = 185;
+		boolean isTall = height > base; // True
+		
+		if (isTall) { // 만약 isTall이 True라면 출력
+			System.out.println("height가 base보다 키가 큽니다.");
+		}
+
+	}
+
+}
