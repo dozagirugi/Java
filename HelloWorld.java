5064ba11ca960b30943f8dfc413e28e612ffbb38
dozagirugi <37648180+kimdoyeonindaeyo@users.noreply.github.com>
2022-10-05 15:41:21 +0900

    Hello World !

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+package javaTest;
+
+public class HelloWorld {
+
+	public static void main(String[] args) { //main 함수는 전체에 하나만 존재 가능 
+		// TODO Auto-generated method stub
+		System.out.println("Hello, Wolrld");
+	}
+
+}
