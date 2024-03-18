5fef1f69309076a863ef00d757ad00518224bfe2
dozagirugi <37648180+kimdoyeonindaeyo@users.noreply.github.com>
2024-03-18 12:22:17 +0900

    Add files via upload
    2024-03-18

---
+++ TypeNumber.java
@@ -0,0 +1,16 @@
+package ch03_1;
+
+public class TypeNumber {
+    public static void main(String[] args){
+        // 'main+Enter' 누르면 메인 메소드가 (윗줄과 같이) 자동 생성된다.
+
+        int a = 10;
+        int b = 5;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a/b);
+        System.out.println(a*b);
+        System.out.println(a+b+15);
+        //'sout' 라고 치면 명령문이 (윗줄과 같이) 자동 완성된다.
+    }
+}
